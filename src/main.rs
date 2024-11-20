134b251115702ec6c472d71ab028985daaaad52f
ultrasigma1488 <andrer.volod@gmail.com>
2024-11-20 17:53:48 +0300

    major refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,158 +1,109 @@
+mod inet_utils;
+mod scic;
+
 use clap::Parser;
-use reqwest::{Client, Error};
-
-use std::time::Instant;
-
-#[derive(Parser, Debug)]
-#[command(long_about = None)]
-struct Args {
-    /// Number of tests
-    #[arg(short, long, value_name = "NUMBER")]
-    tests: Option<u8>,
-
-    /// Enable/Disable download test
-    #[arg(short, value_name = "URL", long)]
-    download: Option<String>,
-
-    /// Size of data to upload
-    #[arg(short, long, default_value_t = 10)]
-    size: usize,
-
-    /// Check the avaibility of the connection
-    #[arg(short, long)]
-    check: bool,
-}
 
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
+  let args = scic::Args::parse();
 
-    if args.check {
-        if is_connected().await {
-            println!("Connection is enable");
-            return;
-        } else {
-            println!("Connection is disable");
-            return;
-        }
+  if args.check {
+    if inet_utils::is_connected().await {
+      println!("Connection can be established!");
+      return;
+    } else {
+      println!("Connection can't be established!");
+      return;
     }
+  }
 
-    let mut download_url_string = String::new();
+  let mut download_url_string = String::new();
 
-    match args.download {
-        Some(ref url) => download_url_string = url.to_string(),
-        None => {}
-    }
-    let download_url_len = download_url_string.len();
+  match args.download {
+    Some(ref url) => download_url_string = url.to_string(),
+    None => {}
+  }
+  let download_url_len = download_url_string.len();
 
-    let upload_url = "https://httpbin.org/post";
+  let upload_url = "https://httpbin.org/post";
 
-    let mut num_tests = 5;
+  let mut num_tests = 5;
 
-    match args.tests {
-        Some(tests) => num_tests = tests,
-        None => print!(""),
-    }
+  match args.tests {
+    Some(tests) => num_tests = tests,
+    None => print!(""),
+  }
 
-    let mut total_download_speed = 0.0;
+  let mut total_download_speed = 0.0;
 
-    if download_url_len != 0 {
-        for i in 1..=num_tests {
-            let value = measure_download_speed(download_url_string.clone());
-            let speed = value.await;
-
-            match speed {
-                Ok(s) => {
-                    total_download_speed += s;
-                    println!("Test {}: Download speed: {:.2} MB/s", i, s);
-                    if s > 0.0 {
-                        println!("----                   ----------");
-                    }
-                }
-                Err(e) => {
-                    eprintln!("Error in measuring load: {}", e);
-                }
-            }
-        }
-
-        let average_download_speed = total_download_speed / num_tests as f64;
-        println!("Avg download speed: {:.2} Mb/s", average_download_speed);
-        println!();
-    }
-
-    let mut total_upload_speed = 0.0;
-
-    println!("Uploading {} MB of data", args.size);
+  if download_url_len != 0 {
     for i in 1..=num_tests {
-        let speed = measure_upload_speed(upload_url).await;
+      let value = inet_utils::measure_download_speed(
+        download_url_string.clone(),
+      );
+      let speed = value.await;
 
-        match speed {
-            Ok(s) => {
-                total_upload_speed += s;
-                println!("Test {}: Upload speed: {:.2} MB/s", i, s);
-                if s < 2.0 {
-                    println!("------               ----------");
-                }
-            }
-            Err(e) => {
-                eprintln!("error in measuring upload {}", e);
-            }
+      match speed {
+        Ok(s) => {
+          total_download_speed += s;
+          println!("Test {}: Download speed: {:.2} MB/s", i, s);
+          if s > 0.0 {
+            println!("----                   ----------");
+          }
         }
+        Err(e) => {
+          eprintln!("Error in measuring load: {}", e);
+        }
+      }
     }
 
-    let average_upload_speed = total_upload_speed / num_tests as f64;
-    println!("Avg upload speed: {:.2} MB/s", average_upload_speed);
+    let average_download_speed =
+      total_download_speed / num_tests as f64;
+    println!(
+      "Avg download speed: {:.2} Mb/s",
+      average_download_speed
+    );
+    println!();
+  }
 
-    match () {
-        _ if average_upload_speed <= 1.0 => {
-            println!("\nHolly Shit, Bro, Your Connection is Shitful")
+  let mut total_upload_speed = 0.0;
+
+  println!("Uploading {} MB of data", args.size);
+  for i in 1..=num_tests {
+    let speed = inet_utils::measure_upload_speed(upload_url).await;
+
+    match speed {
+      Ok(s) => {
+        total_upload_speed += s;
+        println!("Test {}: Upload speed: {:.2} MB/s", i, s);
+        if s < 2.0 {
+          println!("------               ----------");
         }
-        _ if average_upload_speed <= 2.0 && average_upload_speed > 1.0 => {
-            println!("\nNot Bad, But Can be Better")
-        }
-        _ if average_upload_speed > 2.0 && average_upload_speed <= 3.0 => println!("\nIt Will Do"),
-        _ if average_upload_speed > 3.0 => println!("\nGood"),
-        () => todo!(),
+      }
+      Err(e) => {
+        eprintln!("error in measuring upload {}", e);
+      }
     }
-}
+  }
 
-async fn is_connected() -> bool {
-    let responce = reqwest::get("https://www.google.com").await;
+  let average_upload_speed = total_upload_speed / num_tests as f64;
+  println!("Avg upload speed: {:.2} MB/s", average_upload_speed);
 
-    match responce {
-        Ok(res) => res.status().is_success(),
-        Err(_) => false,
+  match () {
+    _ if average_upload_speed <= 1.0 => {
+      println!("\nHolly Shit, Bro, Your Connection is Shitful")
     }
-}
-
-async fn measure_download_speed(url: String) -> Result<f64, Error> {
-    let client = Client::new();
-    let start_time = Instant::now();
-    let response = client.get(url).send().await?.error_for_status()?;
-
-    let content_length = response.content_length().unwrap_or(0);
-    let duration = start_time.elapsed();
-
-    let speed_mbps = (content_length as f64 / 1024.0 / 1024.0) / duration.as_secs_f64();
-    Ok(speed_mbps.clone())
-}
-
-async fn measure_upload_speed(url: &str) -> Result<f64, Error> {
-    let args = Args::parse();
-
-    let client = Client::new();
-    let data = vec![0u8; args.size * 1024 * 1024];
-
-    let start_time = Instant::now();
-    let _response = client
-        .post(url)
-        .body(data.clone())
-        .send()
-        .await?
-        .error_for_status()?;
-
-    let duration = start_time.elapsed();
-    let speed_mbps = (data.len() as f64 / 1024.0 / 1024.0) / duration.as_secs_f64();
-
-    Ok(speed_mbps)
+    _ if average_upload_speed <= 2.0
+      && average_upload_speed > 1.0 =>
+    {
+      println!("\nNot Bad, But Can be Better")
+    }
+    _ if average_upload_speed > 2.0
+      && average_upload_speed <= 3.0 =>
+    {
+      println!("\nIt Will Do")
+    }
+    _ if average_upload_speed > 3.0 => println!("\nGood"),
+    () => todo!(),
+  }
 }
---
+++ src/scic.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[command(long_about = None)]
+pub struct Args {
+  /// Number of tests
+  #[arg(short, long, value_name = "NUMBER")]
+  pub tests: Option<u8>,
+
+  /// Enable/Disable download test
+  #[arg(short, value_name = "URL", long)]
+  pub download: Option<String>,
+
+  /// Size of data to upload
+  #[arg(short, long, default_value_t = 10)]
+  pub size: usize,
+
+  /// Check the avaibility of the connection
+  #[arg(short, long)]
+  pub check: bool,
+}
